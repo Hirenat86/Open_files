5854b6e458ba7e268ec2486abd0aff9a6cf89bbe
Hirenat Dell <hirenat86@gmail.com>
2024-10-10 22:45:08 +0700

    correction of comments

--- Cook_book.py
+++ Cook_book.py
@@ -1,7 +1,8 @@
 from operator import itemgetter
 
-cook_book = {}
+
 with open('recipes.txt', encoding='utf-8') as f:
+    cook_book = {}
     for line in f:
         recepie_name = line.strip()
         ingredients_count = f.readline()
@@ -36,29 +37,26 @@ print()
 
 
 def count_line(*files):
-	list_of_files = []
-	text_f = []		
-	for file in files:
-		with open(file, encoding = 'utf-8') as file_obj:
-			text_f = file_obj.read().splitlines()
-			file_length = len(text_f)
-			name_file = file
-			list_of_files.append([name_file, file_length, text_f])
-			list_of_files.sort(key = itemgetter(1))
-	print(list_of_files)
-	return list_of_files
-
-count_line = count_line('1.txt', '2.txt', '3.txt')
-a = 'result.txt'
+    list_of_files = []
+    for file in files:
+        text_f = []
+        with open(file, encoding = 'utf-8') as file_obj:
+            text_f = file_obj.read().splitlines()
+            file_length = len(text_f)
+            name_file = file
+            text_str = '\n'.join(text_f)
+            list_of_files.append([name_file, file_length, text_str])
+            list_of_files.sort(key = itemgetter(1))
+    return list_of_files
 
 
-def writing_file(list_of_files,a):
-	with open('result.txt', 'w', encoding='utf-8') as file_obj:
-		for file in list_of_files:
-			for element in file:
-				file_obj.write(f'{element}\n')			
-	file_path = 'result.txt'
-	return file_path
+def writing_file(list_of_files, a=''):
+    with open(a, 'w', encoding='utf-8') as file_obj:
+        for file in list_of_files:
+            for element in file:
+                file_obj.write(f'{element}\n')			
+    file_path = a
+    return file_path
 
 
-print(writing_file(count_line, 'result.txt'))
+print(writing_file(count_line('1.txt', '2.txt', '3.txt'), 'result.txt'))
